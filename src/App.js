fb9cb74cef94f67750a415094e90e523a236e2ad
chavvaruchitha123 <ruchithachavvaruchitha09350@gmail.com>
2024-02-20 22:10:50 +0530

    bootstrap1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Corousel from './components/Functional-components/corousel/corousel';
+import ImageComponent from './components/Functional-components/image/image-component';
+import ModalExample from './components/Functional-components/moduls/moduls';
+import TextLinkExample from './components/Functional-components/navbar/navbar';
+import AnimatedToast from './components/Functional-components/toast/toast';
 
 function App() {
   return (
     <div>
-      
-        
-   
-
-    
+      <Corousel></Corousel>
+        <ImageComponent></ImageComponent>
+   <ModalExample></ModalExample>
+<TextLinkExample></TextLinkExample>
+    <AnimatedToast></AnimatedToast>
     </div>
   );
 }
--- src/components/Functional-components/image/image-component.js
+++ src/components/Functional-components/image/image-component.js
@@ -1,6 +1,16 @@
-const ImageComponent=()=>{
-   return(
-    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="img" height={100} width={100}/>
-   ) 
+// const ImageComponent=()=>{
+//    return(
+//     <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="img" height={100} width={100}/>
+//    ) 
+// }
+// export default ImageComponent
+const ImageComponent =({url="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"})=>{
+    return(
+      <div>
+        <img src={url} height={100} width={100} alt="" />
+        <img src={url} height={500} width={"50%"} style={{marginLeft:300}} alt="" />
+        </div>
+    )
 }
-export default ImageComponent
+export  default ImageComponent
+
