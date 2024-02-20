027ea88fbcfd3f723badaf27b90240c2af733d6b
chavvaruchitha123 <ruchithachavvaruchitha09350@gmail.com>
2024-02-20 21:56:07 +0530

    del

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import ButtonComponent from './components/Functional-components/button/button-component';
-import OrderedList from './components/Functional-components/List/order list/orderedlist/orderedlist';
-import UnOrderedList from './components/Functional-components/List/order list/unorder-list';
-import ImageComponent from './components/Functional-components/image/image-component';
-import TableComponent from './components/Functional-components/tables/tablecomponent';
 
 function App() {
   return (
     <div>
       
-        <h1>Hello world</h1>
-    <ButtonComponent></ButtonComponent>
-    <OrderedList></OrderedList>
-    <UnOrderedList></UnOrderedList>
-    <ImageComponent></ImageComponent>
-    <ImageComponent></ImageComponent>
-    <ImageComponent></ImageComponent>
-    <TableComponent></TableComponent>
+        
+   
 
     
     </div>
