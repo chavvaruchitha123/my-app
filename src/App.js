32acd46d472c2f1ce3487e05f390f0c8c9214f55
chavvaruchitha123 <ruchithachavvaruchitha09350@gmail.com>
2024-02-20 21:42:03 +0530

    class based component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import ButtonComponent from './components/Functional-components/button/button-component';
-import OrderedList from './components/Functional-components/List/order list/orderedlist/orderedlist';
-import UnOrderedList from './components/Functional-components/List/order list/unorder-list';
-import ImageComponent from './components/Functional-components/image/image-component';
-import TableComponent from './components/Functional-components/tables/tablecomponent';
+import ImageComponent from './components/classcomponents/imagecomponent';
+import Products from './components/classcomponents/productcomponents/product';
+import YoutubeButton from './components/youtubebutton/youtubebutton';
+
 
 function App() {
   return (
     <div>
       
-        <h1>Hello world</h1>
-    <ButtonComponent></ButtonComponent>
-    <OrderedList></OrderedList>
-    <UnOrderedList></UnOrderedList>
+        
+<Products></Products>
     <ImageComponent></ImageComponent>
-    <ImageComponent></ImageComponent>
-    <ImageComponent></ImageComponent>
-    <TableComponent></TableComponent>
-
-    
+    <YoutubeButton></YoutubeButton>
     </div>
   );
 }
